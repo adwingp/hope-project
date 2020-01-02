0c80d6a41e3e491fbc1ab3a222abfc06c6a6717b
Acube Admin <adwin@acube.co>
2020-01-02 18:19:38 +0530

    initial commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,131 @@
+
+    <!-- footer start -->
+    <footer class="footer">
+        <div class="footer_top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-md-6 col-lg-4">
+                        <div class="footer_widget">
+                            <div class="footer_logo">
+                                <a href="#">
+                                    <img src="public/img/footer_logo.png" alt="">
+                                </a>
+                            </div>
+                            <p>
+                                Firmament morning sixth subdue darkness
+                                creeping gathered divide.
+                            </p>
+                            <div class="socail_links">
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="ti-facebook"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="ti-twitter-alt"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-instagram"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-2 offset-xl-1 col-md-6 col-lg-3">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                Services
+                            </h3>
+                            <ul>
+                                <li><a href="#">Design</a></li>
+                                <li><a href="#">Development</a></li>
+                                <li><a href="#">Marketing</a></li>
+                                <li><a href="#">Consulting</a></li>
+                                <li><a href="#">Finance</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-md-6 col-lg-2">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                Useful Links
+                            </h3>
+                            <ul>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#"> Contact</a></li>
+                                <li><a href="#"> Free quote</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6 col-lg-3">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                Address
+                            </h3>
+                            <ul>
+                                <li>200, D-block, Green lane USA</li>
+                                <li>+10 367 467 8934</li>
+                                <li><a href="#"> docmed@contact.com</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copy-right_text">
+            <div class="container">
+                <div class="footer_border"></div>
+                <div class="row">
+                    <div class="col-xl-12">
+                        <p class="copy_right text-center">
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            Copyright &copy;
+                            <script>document.write(new Date().getFullYear());</script> All rights reserved | This
+                            template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a
+                                href="https://colorlib.com" target="_blank">Colorlib</a>
+                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/ footer end  -->
+
+    <!-- link that opens popup -->
+
+    <!-- form itself end-->
+    <form id="test-form" class="white-popup-block mfp-hide">
+        <div class="popup_box ">
+            <div class="popup_inner">
+                <div class="popup_header">
+                    <h3>Get Free Quote</h3>
+                </div>
+                <div class="custom_form">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <input type="text" placeholder="Your Name">
+                        </div>
+                        <div class="col-xl-12">
+                            <input type="email" placeholder="Email">
+                        </div>
+                        <div class="col-xl-12">
+                            <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                        </div>
+                        <div class="col-xl-12">
+                            <button type="submit" class="boxed-btn3">Send</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    <!-- form itself end -->
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">    
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>busicol</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="public/css/magnific-popup.css">
+    <link rel="stylesheet" href="public/css/font-awesome.min.css">
+    <link rel="stylesheet" href="public/css/themify-icons.css">
+    <link rel="stylesheet" href="public/css/nice-select.css">
+    <link rel="stylesheet" href="public/css/flaticon.css">
+    <link rel="stylesheet" href="public/css/gijgo.css">
+    <link rel="stylesheet" href="public/css/animate.css">
+    <link rel="stylesheet" href="public/css/slick.css">
+    <link rel="stylesheet" href="public/css/slicknav.css">
+    <link rel="stylesheet" href="public/css/style.css">
+    <!-- <link rel="stylesheet" href="public/css/responsive.css"> -->
+    <style>
+      body {
+            /* background-image: url("paper.gif"); */
+            /* background-image: url('public/img/background.png'); */
+            background: #3d8a6352;
+            }
+
+            .panel {
+                    width: 100%;
+                    height: 100%;
+                    background: #798a84;
+                    transform: perspective(600px) rotateY(16deg);
+            }
+
+            .panel_mob {
+                    transform: perspective(600px) rotateY(0deg) !important; 
+                    margin-top: 10px !important;
+            }
+</style>    
+</head>
+<body>
+
+            @include('includes.header')
+            @yield('content')
+            @include('includes.footer')
+
+
+
+    <!-- JS here -->
+    <script src="public/js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="public/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="public/js/popper.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+    <script src="public/js/owl.carousel.min.js"></script>
+    <script src="public/js/isotope.pkgd.min.js"></script>
+    <script src="public/js/ajax-form.js"></script>
+    <script src="public/js/waypoints.min.js"></script>
+    <script src="public/js/jquery.counterup.min.js"></script>
+    <script src="public/js/imagesloaded.pkgd.min.js"></script>
+    <script src="public/js/scrollIt.js"></script>
+    <script src="public/js/jquery.scrollUp.min.js"></script>
+    <script src="public/js/wow.min.js"></script>
+    <script src="public/js/nice-select.min.js"></script>
+    <script src="public/js/jquery.slicknav.min.js"></script>
+    <script src="public/js/jquery.magnific-popup.min.js"></script>
+    <script src="public/js/plugins.js"></script>
+    <script src="public/js/gijgo.min.js"></script>
+    <script src="public/js/slick.min.js"></script>
+    <!--contact js-->
+    <script src="public/js/contact.js"></script>
+    <script src="public/js/jquery.ajaxchimp.min.js"></script>
+    <script src="public/js/jquery.form.js"></script>
+    <script src="public/js/jquery.validate.min.js"></script>
+    <script src="public/js/mail-script.js"></script>
+
+    <script src="public/js/main.js"></script>
+</body>
+</html>
